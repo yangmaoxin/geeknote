1c1c7c407bef5d227827c700e9b7476781d35c15
ymx <ymx0107@gmail.com>
2016-01-25 21:07:54 +0800

    Form auto backup script's commit

--- scripts/backup.js
+++ scripts/backup.js
@@ -14,7 +14,7 @@ function run() {
 		exit(1);
 	} else {
 		echo("======================Auto Backup Begin===========================");
-		cd('F:/geeknote');    //此处修改为Hexo根目录路径
+		cd('E:/geeknote');    //此处修改为Hexo根目录路径
 		if (exec('git add --all').code !== 0) {
 			echo('Error: Git add failed');
 			exit(1);
